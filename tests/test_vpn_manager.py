60a303975792077abd28f0cd1e4c07bdbbdebe22
Zeeshan Ahmad <ahmadzee26@gmail.com>
2024-02-24 16:38:51 +0500

    Update test_vpn_manager.py

--- tests/test_vpn_manager.py
+++ tests/test_vpn_manager.py
@@ -1,1 +1,32 @@
+# test_vpn_manager.py
+
+import unittest
+from unittest.mock import patch
+from src.vpn.vpn_manager import connect_vpn, disconnect_vpn
+
+class TestVpnManager(unittest.TestCase):
+    """
+    This class contains unit tests for the vpn_manager module.
+    """
+
+    @patch('src.vpn.vpn_manager.connect_vpn')
+    def test_connect_vpn(self, mock_connect_vpn):
+        """
+        Test if the connect_vpn function is callable.
+        """
+        connect_vpn()
+        mock_connect_vpn.assert_called()
+
+    @patch('src.vpn.vpn_manager.disconnect_vpn')
+    def test_disconnect_vpn(self, mock_disconnect_vpn):
+        """
+        Test if the disconnect_vpn function is callable.
+        """
+        disconnect_vpn()
+        mock_disconnect_vpn.assert_called()
+
+    # Additional tests can be added for more specific VPN functionalities
+
+if __name__ == '__main__':
+    unittest.main()
 
